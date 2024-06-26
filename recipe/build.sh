5ede8c911f85a749d8ad99a6f3c972037664578f
Lorenzo Pirritano <lpirritano@anaconda.com>
2024-06-26 16:52:06 +0200

    refactor un-vendor lemon/spdlog

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,9 +9,17 @@ cmake . -B build -G Ninja $CMAKE_ARGS  \
         -DBUILD_TESTING=OFF            \
         -DBUILD_PYTHON=ON              \
         -DBUILD_SHARED_LIBS=ON         \
-        -DPython3_EXECUTABLE="$PYTHON"
+        -DPython3_EXECUTABLE="$PYTHON" \
+        -DFETCHCONTENT_TRY_FIND_PACKAGE_MODE=ALWAYS \
+        -DCMAKE_REQUIRE_FIND_PACKAGE_LEMON=ON
 
 cmake --build build --config Release --target all -v
+
+# The build will create a wheel file in build/python/dist
+# we install that using the usual python script.
+# The name of the generated wheel:
+#  e.g. cspy-1.0.3-cp38-cp38-linux_aarch64.whl
+${PYTHON} -m pip install build/python/dist/cspy-*.whl --no-deps --no-build-isolation -vv
 
 # cd build && ctest --verbose -R python_unittest
 
