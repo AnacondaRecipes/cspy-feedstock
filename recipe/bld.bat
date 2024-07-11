91ace6d0582cacc147074cc03946c5c645c7f0ed
Lorenzo Pirritano <lpirritano@anaconda.com>
2024-07-11 15:22:05 +0200

    use "Visual Studio 16 2019"

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,7 @@
 dir %PREFIX%\Library
 
 :: Configure. Switch off unit tests (require gtest / googletest)
-cmake . -B build -G"Ninja" %CMAKE_ARGS%      ^
+cmake . -B build -G"Visual Studio 16 2019" %CMAKE_ARGS%      ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX%  ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX%     ^
     -DCMAKE_BUILD_TYPE=Release               ^
@@ -12,6 +12,10 @@ cmake . -B build -G"Ninja" %CMAKE_ARGS%      ^
     -DFETCHCONTENT_TRY_FIND_PACKAGE_MODE=ALWAYS ^
     -DCMAKE_REQUIRE_FIND_PACKAGE_LEMON=ON
 if errorlevel 1 exit 1
+
+cmake --build build --config Release --target install -v
+if errorlevel 1 exit 1
+
 
 :: Install.
 cmake --install .\build
